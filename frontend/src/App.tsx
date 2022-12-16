ffe33d3e89f18065da67c871b4ba5c38caa960b5
Dominic C. Bernas <nickobernas795@gmail.com>
2022-12-16 11:02:16 +0800

    adding login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Container } from '@mui/material';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Login">
+      <div className="LoginBox">
+        <div className="LoginHeader">Family Vet Clinic And Supplies</div>
+        <div className="inputs">
+          <input className="name" placeholder="Enter your name:"/>
+          <input 
+          className="password" 
+          placeholder="Enter your password:"
+          type="password"
+          />
+        </div>
+        <div style={{display: "flex",justifyContent:"center"}}>
+          <button className="submitbutton">Login</button>
+        </div>
+      </div>
     </div>
+      
   );
 }
 
